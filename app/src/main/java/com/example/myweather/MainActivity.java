50b1cab7f60d9e59009bd20c3d1f910a8988a619
kamilmarkowski <markian99@wp.pl>
2022-06-02 11:48:31 +0200

    1.04

--- app/src/main/java/com/example/myweather/MainActivity.java
+++ app/src/main/java/com/example/myweather/MainActivity.java
@@ -12,20 +12,20 @@ public class MainActivity extends AppCompatActivity {
 
     //private RelativeLayout Homerl;
 
-    private Text wiatr;
-    private Text miasto;
+    //private Text wiatr;
+    //private Text miasto;
 
     private final String url = "http://api.weatherapi.com/v1/forecast.json?key=f5294d8b4e6545018b1184018220106&q=London&days=1&aqi=yes&alerts=yes";
     private final String appid = "f5294d8b4e6545018b1184018220106";
-    DecimalFormat df = new DecimalFormat("#.##");
+    //DecimalFormat df = new DecimalFormat("#.##");
 
-    @SuppressLint("WrongViewCast")
+    //@SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    wiatr = (Text)findViewById(R.id.wiatr);
-    miasto = (Text)findViewById(R.id.miasto);
+    //wiatr = (Text)findViewById(R.id.wiatr);
+   // miasto = (Text)findViewById(R.id.miasto);
 
 
     }
