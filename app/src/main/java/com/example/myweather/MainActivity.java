0c0b2925983f8486a763e80271d983865ab95567
Kamil Markowski <kamilmarkowski@MacBook-Air-Kamil.local>
2022-06-02 08:46:42 +0200

    1.03

--- app/src/main/java/com/example/myweather/MainActivity.java
+++ app/src/main/java/com/example/myweather/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.myweather;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import  androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private RelativeLayout Homerl;
+    private TextView miasto, temperatura;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        miasto = findViewById(R.id.textMiasto);
+        temperatura = findViewById(R.id.textTemperatura);
+
     }
 
 }
---
+++ app/src/main/java/com/example/myweather/WeatherRVAdapter.java
@@ -0,0 +1,45 @@
+package com.example.myweather;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class WeatherRVAdapter extends RecyclerView.Adapter<WeatherRVAdapter.ViewHolder> {
+
+    private Context context;
+
+    public WeatherRVAdapter(Context context, ArrayList<WeatherRVMmode> weatherRVMmodesArrayList) {
+        this.context = context;
+        this.weatherRVMmodesArrayList = weatherRVMmodesArrayList;
+    }
+
+    private ArrayList<WeatherRVMmode> weatherRVMmodesArrayList;
+
+    @NonNull
+    @Override
+    public WeatherRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.weather_rv_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WeatherRVAdapter.ViewHolder holder, int position) {
+
+    }//34 minuta 
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
